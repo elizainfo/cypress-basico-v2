0a649e3723771f1ee0f330bb690decc89e5e455e
Elizangela Santos <elizainfoam@gmail.com>
2023-03-16 15:21:54 -0300

    Concluído Exercicio 1 + Extra 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -16,11 +16,30 @@
 //   })
 
 //Forma do curso:
-  describe('Central de Atendimento ao Cliente TAT', function() {
-    
-      it('verifica o título da aplicação', function() {
-        cy.visit('./src/index.html')
-        
+  describe('Central de Atendimento ao Cliente TAT', () => {
+    beforeEach(function() {
+      cy.visit('./src/index.html')
+    })
+
+      it('verifica o título da aplicação', function() {        
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
       })
-    })
+
+      it('preenche os campos obrigatórios e envia o formulário', function(){
+        const longText = 'Testando o preenchimento dos campos obrigatórios se utilizando da propridade de delay durante o preenchimento'
+        cy.get('#firstName').type('NovoNome')
+        .should('have.value', 'NovoNome')
+        cy.get('#lastName').type('NovoSobreNome')
+        .should('have.value', 'NovoSobreNome')
+        cy.get('#email').type('teste@teste.com')
+        .should('have.value', 'teste@teste.com')
+        cy.get('#phone').type('1147736767')
+        .should('have.value', '1147736767')
+        cy.get('#open-text-area').type(longText, {delay:0})
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success > strong').should('be.visible')
+       // cy.get('.sucess')
+                
+      })  
+  })
